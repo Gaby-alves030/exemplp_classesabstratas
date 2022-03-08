25bf0600f6183b995993936b82b6f457afeadebb
Gaby <gaabyrocha574@gmail.com>
2022-03-08 17:54:52 +0000

    aula java

---
+++ Aguia.java
@@ -0,0 +1,21 @@
+public class Aguia extends Ave {
+
+    @Override
+    public String voar() {
+        //TODO Auto-generated method stub
+    return "nada mal";
+
+    }
+    @Override
+    public String nadar() {
+         //TODO Auto-generated method stub
+         return "voa bem";
+    }
+
+
+    public void mensagem() {
+        System.out.println("");
+    }
+
+
+}
---
+++ Principal.java
@@ -0,0 +1,19 @@
+public class Principal {
+    public static void main(String[] args) {
+        
+
+
+        Papagaio p=new Papagaio();
+        Aguia a=new Aguia();
+
+        a.setNome("Corinthians");
+        System.out.println(a.voar());
+
+
+        p.setNome("Loro");
+        System.out.println(p.voar());
+
+        
+    }
+    
+}
